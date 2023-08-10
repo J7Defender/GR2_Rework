da48f357942eceaa688d171dc07278ff357f3a5a
Duy Pham <72265557+J7Defender@users.noreply.github.com>
2023-08-10 10:46:29 +0700

    Revert "* Comment and reorganize Character"
    This reverts commit 4e6b96536649f9c39786acaddffbd09e769b4e09.

--- Source/GR2_Rework/Character/GR2_ReworkCharacter.h
+++ Source/GR2_Rework/Character/GR2_ReworkCharacter.h
@@ -20,11 +20,6 @@ class AGR2_ReworkCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-public:
-	AGR2_ReworkCharacter();
-	
-	virtual void BeginPlay() override;
-
 	/** Pawn mesh: 1st person view (arms; seen only by self) */
 	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
 	USkeletalMeshComponent* Mesh1P;
@@ -33,22 +28,9 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FirstPersonCameraComponent;
 
-	/** Pawn mesh: 3rd person view */
-	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category=Mesh, Replicated)
-	USkeletalMeshComponent* Mesh3P;
-
 	/** MappingContext */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
 	class UInputMappingContext* DefaultMappingContext;
-	
-	/** Returns Mesh1P subobject **/
-	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
-	
-	/** Returns FirstPersonCameraComponent subobject **/
-	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
-
-	// ----------------------------------------------------------------
-	// Actions
 
 	/** Jump Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
@@ -61,6 +43,22 @@ public:
 	/** Move Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
 	class UInputAction* MoveAction;
+
+public:
+	AGR2_ReworkCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+
+	/** Pawn mesh: 3rd person view */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category=Mesh, Replicated)
+	USkeletalMeshComponent* Mesh3P;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapons")
+	bool bIsMoveAble = false;
+	// TODO: Change bIsMoveAble to false to prevent players from moving while choosing team
 		
 	/** Look Input Action */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
@@ -82,34 +80,11 @@ public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UInputAction* ReloadAction;
 
-	// ----------------------------------------------------------------
-	// Basic Controls
-	
-	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
-	
-	/** Called for movement input */
-	void Move(const FInputActionValue& Value);
-
-	/** Called for looking input */
-	void Look(const FInputActionValue& Value);
-
-	/** Called for crouch action*/
-	void StartCrouch();
-
-	/** Called for stop crouch action*/
-	void StopCrouch();
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapons")
-	bool bIsMoveAble = false;
-	// TODO: Change bIsMoveAble to false to prevent players from moving while choosing team
-
-	/** Bool for Animation to know when holding gun */
+	/** Bool for AnimBP to switch to another animation set */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Weapon)
 	bool bHasRifle;
 
-	// ----------------------------------------------------------------
-	// Movement Affect
-	
+private:
 	float MovementSpeedMultiplier = 1.0f;
 	
 	FTimerHandle TimerHandle_SlowEffectTimer;
@@ -123,8 +98,15 @@ public:
 	void FinishSlowdown();
 	void SlowDownOnHit();
 
-	// ----------------------------------------------------------------
-	// Damage Dealt
+public:
+
+	/** Setter to set the bool */
+	UFUNCTION(BlueprintCallable, Category = Weapon)
+	void SetHasRifle(bool bNewHasRifle);
+
+	/** Getter for the bool */
+	UFUNCTION(BlueprintCallable, Category = Weapon)
+	bool GetHasRifle();
 	
 	void DealDamage(FHitResult HitResult, int Damage, FVector HitFrom, AGR2_ReworkCharacter* DamageCauser);
 
@@ -132,16 +114,6 @@ public:
 	void Server_OnDealDamage(FHitResult HitResult, int Damage, FVector HitFrom, AGR2_ReworkCharacter* DamageCauser);
 	bool Server_OnDealDamage_Validate(FHitResult HitResult, int Damage, FVector HitFrom, AGR2_ReworkCharacter* DamageCauser);
 	void Server_OnDealDamage_Implementation(FHitResult HitResult, int Damage, FVector HitFrom, AGR2_ReworkCharacter* DamageCauser);
-	
-	/** Event for taking damage. Overridden from APawn.*/
-	UFUNCTION(BlueprintCallable, Category = "Health")
-	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Damage")
-	AGR2_ReworkCharacter* LastDamageDealer;
-
-	// ----------------------------------------------------------------
-	// Effects Handlers
 
 	UFUNCTION(Server, Unreliable)
 	void Server_PlayFireSound(USoundBase* FFireSound);
@@ -167,8 +139,28 @@ public:
 	UFUNCTION(NetMulticast, Unreliable)
 	void Multi_SpawnImpactFX();
 
-	//----------------------------------------------------------------
-	// Weapons and Weapon Actions
+	UFUNCTION(BlueprintCallable, Category="Restart")
+	void RestartPlayerOnBeginMatch();
+
+	UFUNCTION()
+	void SetPlayerColor(const FString& Color);
+
+	virtual void OnRep_PlayerState() override;
+
+protected:
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	/** Called for crouch action*/
+	void StartCrouch();
+
+	/** Called for stop crouch action*/
+	void StopCrouch();
+
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const override;
 
 	void OnPrimaryWeaponChoose();
 	void OnSecondaryWeaponChoose();
@@ -176,20 +168,18 @@ public:
 	void StopBurst();
 
 	void OnReloadWeapon();
+	
+	// APawn interface
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	// End of APawn interface
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapons", ReplicatedUsing = OnRep_CurrentWeapon)
-	AGR2_ReworkWeapon* CurrentWeapon;
+public:
+	/** Returns Mesh1P subobject **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	
+	/** Returns FirstPersonCameraComponent subobject **/
+	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
 
-	/** Current weapons of player */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapons", Replicated)
-	AGR2_ReworkWeapon* Weapon1;
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapons", Replicated)
-	AGR2_ReworkWeapon* Weapon2;
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapons", Replicated)
-	AGR2_ReworkWeapon* Weapon3;
-	
 	/** Get Current Weapon*/
 	AGR2_ReworkWeapon* GetCurrentWeapon() const { return CurrentWeapon; }
 
@@ -208,20 +198,7 @@ public:
 	/** Set Secondary Weapon */
 	void SetWeapon2(AGR2_ReworkWeapon* Weapon) { Weapon2 = Weapon; }
 
-	UFUNCTION(BlueprintNativeEvent, Category="Weapon")
-	void OnCurrentWeaponSet(AGR2_ReworkWeapon* WeaponToSet);
-
-	UFUNCTION(BlueprintNativeEvent, Category="Weapon")
-	void SetWeaponOnSpawn();
-	
-	void Server_DestroyWeapons();
-	void DestroyWeapons();
-
-	UFUNCTION(NetMulticast, Reliable, Category="Weapons")
-	void Multi_DestroyWeapons();
-
-	//----------------------------------------------------------------
-	// Health Management
+protected:
 
 	/** The player's maximum health. This is the highest value of their health can be. This value is a value of the player's health, which starts at when spawned.*/
 	UPROPERTY(EditDefaultsOnly, Category = "Health")
@@ -230,6 +207,29 @@ public:
 	/** The player's current health. When reduced to 0, they are considered dead.*/
 	UPROPERTY(ReplicatedUsing = OnRep_CurrentHealth)
 	float CurrentHealth;
+
+public:
+
+	/** Character Blueprint */
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Blueprint")
+	AGR2_ReworkCharacter* CharacterBlueprint;
+
+	/** Character HUD */
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "HUD")
+	UUserWidget* CharacterHUD;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapons", ReplicatedUsing = OnRep_CurrentWeapon)
+	AGR2_ReworkWeapon* CurrentWeapon;
+
+	/** Current weapons of player */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapons", Replicated)
+	AGR2_ReworkWeapon* Weapon1;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapons", Replicated)
+	AGR2_ReworkWeapon* Weapon2;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Weapons", Replicated)
+	AGR2_ReworkWeapon* Weapon3;
 	
 	UFUNCTION(BlueprintNativeEvent, Category="Health")
 	void OnHealthUpdateUI();
@@ -239,9 +239,24 @@ public:
 	
 	UFUNCTION(Server, Reliable)
 	void OnDeathServer(bool bIsInitialSpawn = false);
+	
+	void Server_DestroyWeapons();
+	void DestroyWeapons();
+
+	UFUNCTION(NetMulticast, Reliable, Category="Weapons")
+	void Multi_DestroyWeapons();
+
+	// UFUNCTION()
+	// static void ServerRestartPlayer(AGR2_ReworkGameMode* GameMode, AController* CurrentController);
 
 	UFUNCTION(BlueprintNativeEvent, Category="Health")
 	void OnDeathUpdateUI();
+
+	UFUNCTION(Client, Reliable, Category="Team")
+	void Client_DisplayChooseTeamWidget();
+
+	UFUNCTION(BlueprintNativeEvent, Category="Team")
+	void DisplayChooseTeamWidget();
 	
 	/** RepNotify for changes made to current health.*/
 	UFUNCTION()
@@ -249,6 +264,14 @@ public:
 
 	UFUNCTION()
 	void OnRep_CurrentWeapon();
+
+	UFUNCTION(BlueprintNativeEvent, Category="Weapon")
+	void OnCurrentWeaponSet(AGR2_ReworkWeapon* WeaponToSet);
+
+	UFUNCTION(BlueprintNativeEvent, Category="Weapon")
+	void SetWeaponOnSpawn();
+
+	// Setter & Getter
 	
 	/** Getter for Max Health.*/
 	UFUNCTION(BlueprintPure, Category="Health")
@@ -261,40 +284,11 @@ public:
 	/** Setter for Current Health. Clamps the value between 0 and MaxHealth and calls OnHealthUpdate. Should only be called on the server.*/
 	UFUNCTION(BlueprintCallable, Category="Health")
 	void SetCurrentHealth(float healthValue);
+	
+	/** Event for taking damage. Overridden from APawn.*/
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
 
-	//----------------------------------------------------------------
-	// Relevant Variables
-
-	/** Character Blueprint (self) */
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Blueprint")
-	AGR2_ReworkCharacter* CharacterBlueprint;
-
-	/** Character HUD */
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "HUD")
-	UUserWidget* CharacterHUD;
-
-	//----------------------------------------------------------------
-	// HUD Showing
-
-	UFUNCTION(Client, Reliable, Category="Team")
-	void Client_DisplayChooseTeamWidget();
-
-	UFUNCTION(BlueprintNativeEvent, Category="Team")
-	void DisplayChooseTeamWidget();
-
-	//----------------------------------------------------------------
-	// Replication
-
-	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const override;
-
-	//----------------------------------------------------------------
-	// PlayerStates and Miscs
-
-	UFUNCTION(BlueprintCallable, Category="Restart")
-	void RestartPlayerOnBeginMatch();
-
-	UFUNCTION()
-	void SetPlayerColor(const FString& Color);
-
-	virtual void OnRep_PlayerState() override;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Damage")
+	AGR2_ReworkCharacter* LastDamageDealer;
 };
